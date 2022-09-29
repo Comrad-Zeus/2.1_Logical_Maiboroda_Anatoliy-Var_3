505c663774420a7f4f0701e201f9b7e7b9470724
maybo <maybo@DESKTOP-Q2ESNAL>
2022-09-29 13:35:19 +0200

    Добавьте файлы проекта.

---
+++ 2.1 Логические Выражения Майборода Анатолий/Program.cs
@@ -0,0 +1,8 @@
+﻿// Логические выражения - 3 вариант - Высокий уровень
+
+Console.WriteLine("Введите X: ");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+double y = double.Parse(Console.ReadLine());
+if (y >= 0 && y <= -x * x + 2) Console.WriteLine("Принадлежит");
+else Console.WriteLine("Мимо");
